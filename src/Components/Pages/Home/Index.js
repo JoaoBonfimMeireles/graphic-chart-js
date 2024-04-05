26c78fe584269bc40ebd641ae80ac6ba4d464f83
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-04-05 01:43:53 -0300

    projeto finalizado

--- src/Components/Pages/Home/Index.js
+++ src/Components/Pages/Home/Index.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return(
         <div>
-            teste
+            <img src="https://amildacabecaaospes.com.br/images/foto-secao.png" />
         </div>
     )
 }
